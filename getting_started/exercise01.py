5f2089c53986b4883117f25f67d750f360f286be
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-16 20:13:34 -0300

    update exe01

--- getting_started/exercise01.py
+++ getting_started/exercise01.py
@@ -2,10 +2,14 @@ item1 = "Phone"
 item1_price = 100
 item1_quantity = 5
 item1_price_total = item1_price * item1_quantity
+random_str = "4"
+random_int = int("4")
 
 print(type(item1)) # <class 'str'>
 print(type(item1_price)) # <class 'int'>
 print(type(item1_quantity)) # <class 'int'>
 print(type(item1_price_total)) # <class 'int'>
+print(type(random_str)) # <class 'str'>
+print(type(random_int)) # <class 'int'>
 
 # In python everything is a Class, we could also create our own Class
