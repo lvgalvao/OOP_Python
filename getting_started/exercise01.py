540874eb7b2bb7d7270028d9fb3c331eaa7d7902
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-16 19:58:41 -0300

    exe 01

---
+++ getting_started/exercise01.py
@@ -0,0 +1,11 @@
+item1 = "Phone"
+item1_price = 100
+item1_quantity = 5
+item1_price_total = item1_price * item1_quantity
+
+print(type(item1)) # <class 'str'>
+print(type(item1_price)) # <class 'int'>
+print(type(item1_quantity)) # <class 'int'>
+print(type(item1_price_total)) # <class 'int'>
+
+# In python everything is a Class, we could also create our own Class
