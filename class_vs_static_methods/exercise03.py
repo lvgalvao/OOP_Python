79be8bc55ad95c7a03d1ef7921fef47b7f95751d
Luciano Filho <lvgalvaofilho@gmail.com>
2022-12-17 00:20:38 -0300

    add class

---
+++ class_vs_static_methods/exercise03.py
@@ -0,0 +1,65 @@
+import csv
+
+class Item:
+    pay_rate = 0.8
+    all = []
+
+    def __init__(self, name: str, price: float, quantity:int=0) -> None:
+        # Run validations to the received aruments
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('class_vs_static_methods/items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            Item(
+                name = item.get('Name'),
+                price = float(item.get('Price')),
+                quantity = float(item.get('Quantity')),
+            )
+
+    @staticmethod    
+    def is_integer(num):
+        # We will count out the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
+
+# for instance in Item.all:
+#     print(instance.name)
+
+# Item.instantiate_from_csv()
+# print(Item.all)
+
+# for item in Item.all:
+#     print(item)
+
+
+print(Item.is_integer(5.0))
---
+++ class_vs_static_methods/helper.py
@@ -0,0 +1,9 @@
+from .item import Item
+
+class Phone(Item):
+    pass
+
+phone1 = Phone("JscProhnev10", 500, 5)
+phone1.broken_phones = 1
+phone2 = Phone("JscProhnev20", 700, 5)
+phone2.broken_phones = 1
